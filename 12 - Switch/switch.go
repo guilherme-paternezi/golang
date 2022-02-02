63feeca95d7a2e8a7ebc7c361dc84d77f7c82fda
guilherme-paternezi <guilherme.paternezi@bandtec.com.br>
2022-02-02 15:27:58 -0300

    switch

---
+++ 12 - Switch/switch.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func diaDaSemana(numero int) string {
+	switch numero {
+	case 1:
+		return "Domingo"
+	case 2:
+		return "Segunda"
+	case 3:
+		return "Terça"
+	case 4:
+		return "Quarta"
+	case 5:
+		return "Quinta"
+	case 6:
+		return "Sexta"
+	case 7:
+		return "Sabado"
+	default:
+		return "Numero Ivalido"
+	}
+}
+
+func main() {
+	fmt.Println("SWITCH")
+
+	dia := diaDaSemana(5)
+	fmt.Println(dia)
+}
