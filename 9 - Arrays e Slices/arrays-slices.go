940b66a275b3456370bab7e9440389e6d7d27369
guilherme-paternezi <guilherme.paternezi@bandtec.com.br>
2022-02-02 15:28:06 -0300

    arrays

--- 9 - Arrays e Slices/arrays-slices.go
+++ 9 - Arrays e Slices/arrays-slices.go
@@ -29,4 +29,23 @@ func main() {
 
 	array2[1] = 9
 	fmt.Println()
+
+	//Arrays Internos
+	slice3 := make([]float32, 10, 11)
+	fmt.Println(slice3)
+	fmt.Println(len(slice3))
+	fmt.Println(cap(slice3))
+
+	slice3 = append(slice3, 5)
+	slice3 = append(slice3, 7)
+
+	fmt.Println(slice3)
+	fmt.Println(len(slice3))
+	fmt.Println(cap(slice3))
+
+	slice4 := make([]float32, 5)
+	fmt.Println(slice4)
+	slice4 = append(slice4, 10)
+	fmt.Println(len(slice4))
+	fmt.Println(cap(slice4))
 }
